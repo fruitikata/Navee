3643b3319639c94527bb79bc74bfcf0d446fc3c5
Kali <liyangnriyang@gmail.com>
2025-03-22 22:56:57 +0800

    initial

---
+++ php/get_loc.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "navee";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT latitude, longitude FROM locations ORDER BY timestamp DESC LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode(["lat" => $row["latitude"], "lon" => $row["longitude"]]);
+} else {
+    echo json_encode(["lat" => 0, "lon" => 0]);
+}
+
+$conn->close();
+?>
